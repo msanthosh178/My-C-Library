41ed2d8f2ae5cd4f1b5a1255beb57d16308b144f
Santhosh Reddy Masireddy <msanthosh178@gmail.com>
2017-08-12 10:45:19 -0400

    This application is the Excat replica of facebook application developed on csharp

---
+++ facebooklikeapplication/facebooklikeapplication/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace facebooklikeapplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            likeapplication();
+        }
+
+       public static void likeapplication()
+        {
+            var names = new List<string>();
+
+            while(true)
+            {
+                string s1;
+                Console.WriteLine("Enter the name of the person");
+                s1=Console.ReadLine();
+                names.Add(s1);
+
+                if(names.Count()==1)
+                {
+                    Console.WriteLine(names[0]+" "+"Liked your picture");
+                }
+
+                else if (names.Count()==2)
+                {
+                    Console.WriteLine(names[0]+","+names[1]+" "+"Liked your picture");
+                }
+
+                else if(names.Count()>3)
+                {
+                    Console.WriteLine("{0},{1} and {2} others liked your photo",names[0],names[1],names.Count()-1);
+                }
+                  
+            }
+        }
+
+    }
+}
